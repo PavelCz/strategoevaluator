7dde8608ef255c358ae298a703bff4bb8fad655a
Pavel C <valep.pave@gmx.de>
2017-07-02 19:55:24 +0200

    Refactor extract piece adding into method

--- manager/game.cpp
+++ manager/game.cpp
@@ -247,34 +247,27 @@ void Game::LoadSetup() {
 	int usedUnits[(int)(Piece::BOMB)];
 	for (int ii = 0; ii <= (int)(Piece::BOMB); ++ii)
 		usedUnits[ii] = 0;
+	AddPiecesFromArrayToBoard(redSetup, Piece::RED, usedUnits);
+	AddPiecesFromArrayToBoard(blueSetup, Piece::BLUE, usedUnits);
+	if (usedUnits[(int) Piece::FLAG] <= 0) {
+		; // Error: You need to include a flag!
+	}
+}
+
+void Game::AddPiecesFromArrayToBoard(const char array[][], Piece::Colour colour,
+		const int usedUnits[]) {
 	for (int y = 0; y < height; ++y) {
 		for (int x = 0; x < Game::theGame->theBoard.Width(); ++x) {
-			Piece::Type type = Piece::GetType(redSetup[x][y]);
+			Piece::Type type = Piece::GetType(array[x][y]);
 			if (type != Piece::NOTHING) {
 				usedUnits[(int) (type)]++;
 				if (usedUnits[type] > Piece::maxUnits[(int) type]) {
 					//fprintf(stderr, "Too many units of type %c\n", Piece::tokens[(int)(type)]);
 					// Error: Too many units
 				}
-				Game::theGame->theBoard.AddPiece(x, y, type, Piece::RED);
+				Game::theGame->theBoard.AddPiece(x, y, type, colour);
 			}
 		}
-	}
-	for (int y = 0; y < height; ++y) {
-			for (int x = 0; x < Game::theGame->theBoard.Width(); ++x) {
-				Piece::Type type = Piece::GetType(blueSetup[x][y]);
-				if (type != Piece::NOTHING) {
-					usedUnits[(int) (type)]++;
-					if (usedUnits[type] > Piece::maxUnits[(int) type]) {
-						//fprintf(stderr, "Too many units of type %c\n", Piece::tokens[(int)(type)]);
-						// Error: Too many units
-					}
-					Game::theGame->theBoard.AddPiece(x, y, type, Piece::BLUE);
-				}
-			}
-		}
-	if (usedUnits[(int) Piece::FLAG] <= 0) {
-		; // Error: You need to include a flag!
 	}
 }
 
--- manager/game.h
+++ manager/game.h
@@ -42,6 +42,7 @@ class Game
 	private:
 		void MakeControllers(const char * redPath, const char * bluePath); //Create a controller based off a path
 		void LoadSetup();
+		void AddPiecesFromArrayToBoard(const char array [][], Piece::Colour colour, const int usedUnits[]);
 	public:
 		int logMessage(const char * format, ...);
 		static void PrintResults(const MovementResult & result, std::string & buffer);
